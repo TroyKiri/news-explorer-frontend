0cb9610b37d7251cc8dbb6f57aa991ba6b9c1a6c
Kirill <V-Troitsky@yandex.ru>
2020-10-19 15:30:08 +0300

    add event - open&close menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+
+const header = document.querySelector('.header');
+const menu = document.querySelector('.menu');
+const menuButtonOpen = header.querySelector('.header__icon');
+const menuButtonClose = menu.querySelector('.header__icon');
+
+// Функции для открытия меню (для 320px)
+const openMenu = function (event) {
+  menu.style.display = 'flex';
+};
+const closeMenu = function () {
+  menu.style.display = 'none';
+};
+
+// Открытие и закрытие меню
+menuButtonOpen.addEventListener('click', openMenu);
+menuButtonClose.addEventListener('click', closeMenu);
